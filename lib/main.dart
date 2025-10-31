85be74d549edbcc090051c5c686c0c3e4574f528
jiwon0412 <kimjiwon041226@naver.com>
2025-11-01 00:50:23 +0900

    세부사항(íìfeat :  택(팀장)페이지  구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lifematch_frontend/features/team_management/screens/team_detail_screen.dart';
 import 'features/auth/screens/login_screen.dart';
 import 'features/auth/screens/signup_screen.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
 
       // ✅ 앱 시작 시 LoginScreen으로 이동
-      home: const LoginScreen(),
+      home: const TeamDetailScreen(),
 
       // ✅ 네비게이션 라우트 등록
       routes: {
